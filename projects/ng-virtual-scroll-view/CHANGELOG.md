90a8949e0fdbe146cf5fc1ffe1e3d2a9f2b7e87c
Evgenii Grebennikov <djonnyx@gmail.com>
2026-07-13 16:07:33 +0300

    hotfix: 22.x missing modules were exported

--- projects/ng-virtual-scroll-view/CHANGELOG.md
+++ projects/ng-virtual-scroll-view/CHANGELOG.md
@@ -1,7 +1,7 @@
 # Change Log
 All notable changes to this project will be documented in this file.
 
-## [22.12.4] - 2026-07-13
+## [22.12.5] - 2026-07-13
 
 ### Add
 - `ng-virtual-scroll-view` has been implemented
--- projects/ng-virtual-scroll-view/src/lib/directives/index.ts
+++ projects/ng-virtual-scroll-view/src/lib/directives/index.ts
@@ -1,7 +1,11 @@
+import { LocaleSensitiveDirective } from './locale-sensitive/locale-sensitive.directive';
 import { LocaleSensitiveModule } from './locale-sensitive/locale-sensitive.module';
+import { VirtualClickDirective } from './virtual-click/virtual-click.directive';
 import { VirtualClickModule } from './virtual-click/virtual-click.module';
 
 export {
+    LocaleSensitiveDirective,
     LocaleSensitiveModule,
+    VirtualClickDirective,
     VirtualClickModule,
 }
