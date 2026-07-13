fb70aa2633f20dd0356f192d5cd0d0a7a92979c8
Evgenii Grebennikov <djonnyx@gmail.com>
2026-07-13 15:33:35 +0300

    hotfix: 20.x missing modules were exported

--- projects/ng-virtual-scroll-view/src/lib/ng-virtual-scroll-view.module.ts
+++ projects/ng-virtual-scroll-view/src/lib/ng-virtual-scroll-view.module.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 import { NgVirtualScrollViewComponent } from './ng-virtual-scroll-view.component';
 import { LocaleSensitiveModule } from './directives';
 import { NgScrollerModule } from './components/ng-scroller/ng-scroller.module';
+import { SCROLL_VIEW_SERVICE } from './components/ng-scroll-view/const';
+import { NgVirtualScrollViewService } from './ng-virtual-scroll-view.service';
 
 @NgModule({
   declarations: [NgVirtualScrollViewComponent],
   exports: [NgVirtualScrollViewComponent],
   imports: [CommonModule, NgScrollerModule, LocaleSensitiveModule],
+  providers: [{ provide: SCROLL_VIEW_SERVICE, useClass: NgVirtualScrollViewService }],
   schemas: [NO_ERRORS_SCHEMA],
 })
 export class NgVirtualScrollViewModule { }
