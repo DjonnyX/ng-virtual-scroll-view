c1bdfd887bebf93961768430acd05a28f7c190b3
Evgenii Grebennikov <djonnyx@gmail.com>
2026-07-19 14:09:45 +0300

    hotfix: 21x animation and scrolling defects have been fixed

--- projects/ng-virtual-scroll-view/CHANGELOG.md
+++ projects/ng-virtual-scroll-view/CHANGELOG.md
@@ -1,6 +1,11 @@
 # Change Log
 All notable changes to this project will be documented in this file.
 
+## [21.12.8] - 2026-07-19
+
+### Fix
+- Animation and scrolling defects have been fixed.
+
 ## [21.12.6] - 2026-07-17
 
 ### Fix
--- projects/ng-virtual-scroll-view/src/lib/components/ng-scroll-view/base/base-scroll-view.component.ts
+++ projects/ng-virtual-scroll-view/src/lib/components/ng-scroll-view/base/base-scroll-view.component.ts
@@ -12,7 +12,7 @@ import { TextDirections } from '../../../enums';
  * BaseScrollView
  * Maximum performance for extremely large lists.
  * It is based on algorithms for virtualization of screen objects.
- * @link https://github.com/DjonnyX/ng-virtual-grid/blob/21.x/projects/ng-virtual-grid/src/lib/components/ng-scroll-view/base/base-scroll-view.component.ts
+ * @link https://github.com/DjonnyX/ng-virtual-scroll-view/blob/21.x/projects/ng-virtual-scroll-view/src/lib/components/ng-scroll-view/base/base-scroll-view.component.ts
  * @author Evgenii Alexandrovich Grebennikov
  * @email djonnyx@gmail.com
  */
